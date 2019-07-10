5ee34c3728677ae7c75af868b6786a84a49fd44f
Valentino Wong <valentino.wong@gmail.com>
2019-07-10 15:04:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,8 +213,10 @@ end
 def big_shoe_rebounds
   player = ""
   game_hash.each do |location, team_data|
-    team_data[:players].reduce do |memo,player|
-      binding.pry
+    team_data[:players].each do |player|
+      player.each do |name,stats|
+        binding.pry
+      end
     end
   end
 end
