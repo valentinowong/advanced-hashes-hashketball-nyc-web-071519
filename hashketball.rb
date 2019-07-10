1e80917b3959c3ccadf449502717bfe30cc027e7
Valentino Wong <valentino.wong@gmail.com>
2019-07-10 14:30:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -94,11 +94,11 @@ def game_hash
           "DeSagna Diop" => {
             :number => 2,
             :shoe => 14,
-            :points => 12,
-            :rebounds => 4,
-            :assists => 7,
-            :steals => 22,
-            :blocks => 15,
+            :points => 24,
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 4,
+            :blocks => 5,
             :slam_dunks => 5
           }
         }, {
