7d7066e04ce5b0c54685ecfed92b58ca51aaddd1
Valentino Wong <valentino.wong@gmail.com>
2019-07-10 14:19:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,7 +148,7 @@ end
 
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
-    team_data[:players].each do |attribute, data|
+    team_data[:players].each do |player|
       binding.pry
     end
   end
